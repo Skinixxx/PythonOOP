1b10f911cf075d1543e6d5e5159c3c8016eb2b3c
nikolay_tamindarov <kolyatamindarov@gmail.com>
2025-11-10 15:07:38 +0300

    Complete BankAcc

--- BankAccount.py
+++ BankAccount.py
@@ -1,4 +1,23 @@
 class BankAccount:
     def __init__(self,owner,balance=0):
         self.owner = owner
-        self.balance = balance
+        self.balance = max(balance,0)
+
+    def deposit(self,amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Пополнение суммой: {amount}. Баланс: {self.balance}")
+        else:
+            print(f"Сумма должна быть положительной")
+    
+    def withdraw(self,amount):
+        if (self.balance - amount)>=0:
+            self.balance -= amount
+            print(f"Снятие:{amount}. Баланс: {self.balance}")
+        else:
+            print("Недостаточно средств")
+
+test = BankAccount("Иван",100)
+test.withdraw(200)
+test.deposit(300)
+test.withdraw(400)
